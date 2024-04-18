b1a138169e0bc750e925af49a06a82b419f1440c
Minoddin <techmino99@gmail.com>
2024-04-18 21:19:46 +0600

    add total price

--- js/sub.js
+++ js/sub.js
@@ -1,3 +1,32 @@
-function OmmitDisable(){
-  document.getElementById('ommitDisable').classList.remove('disabled');
-}
+function addBackgroundById(elementId){
+  document.getElementById(elementId).classList.add('bg-red-700');
+
+  const currentNoOfSeat = document.getElementById('currentNoOfSeat');
+  const selectedSeatText = currentNoOfSeat.innerText;
+  const noOfSelectedSeat = parseInt(selectedSeatText);
+  // console.log(noOfSelectedSeat);
+  const newNumberOfSeat = noOfSelectedSeat+1;
+  currentNoOfSeat.innerText = newNumberOfSeat;
+  document.getElementById('totalPrice').innerText = newNumberOfSeat*550;
+
+
+  if (newNumberOfSeat > 4){
+    currentNoOfSeat.innerText = 4;
+   
+    document.getElementById(elementId).classList.remove('bg-red-700');
+    alert ("YOU ARE ALLOWED ONLY FOR FOUR (4 TICKET");
+
+
+  }
+
+
+
+  console.log(totalPriceInt);
+  // const totalPriceText = totalPrice.innerText;
+  // const totalPriceInt = parseInt(totalPriceText);
+
+  // const finaltotalPriceInt = totalPriceInt*2;
+  // totalPrice = finaltotalPriceInt.innerText;
+
+
+}  
